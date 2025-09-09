06db9a9e96d745febdb3eb78b9557ad33350aa63
ehsanKhan <iamehsanalaee006@gmail.com>
2025-09-09 17:43:44 +0330

    get html template on the project

---
+++ OnlineBoutique/OnlineBoutique/Pages/Item.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineBoutique.Pages
+{
+    public class ItemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
